0804e6c7fcd315bdd8332813571b63534746f842
artemis-beta <KXZ186@student.bham.ac.uk>
2017-02-19 22:53:03 +0000

    Pip file structure

--- enigma/reflector.py
+++ enigma/reflector.py
@@ -2,12 +2,13 @@ class reflector:
     def __init__(self):
         self._reflector_dict = { 'A' : 'P', 'B' : 'Z', 'C' : 'H',
                                  'D' : 'S', 'E' : 'Y', 'F' : 'U',
-                                 'G' : 'N', 'H' : 'J', 'K' : 'B',
+                                 'G' : 'N', 'H' : 'M', 'K' : 'B',
                                  'L' : 'F', 'M' : 'D', 'N' : 'K',
-                                 'O' : 'H', 'P' : 'L', 'Q' : 'C',
-                                 'R' : 'T', 'S' : 'W', 'T' : 'P',
+                                 'O' : 'J', 'P' : 'L', 'Q' : 'C',
+                                 'R' : 'T', 'S' : 'W', 'T' : 'X',
                                  'U' : 'A', 'V' : 'E', 'W' : 'I',
-                                 'X' : 'R', 'Y' : 'O', 'Z' : 'G'}
+                                 'X' : 'R', 'Y' : 'O', 'Z' : 'G',
+				 'I' : 'Q', 'J' : 'V'}
         
     def reflector_conversion(self, letter):
         return self._reflector_dict[letter]
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name                =  'enigma'                                      ,
+      version             =  '0.1.0'                                       ,
+      description         =  'Enigma Machine Emulator for Python.'         ,
+      url                 =  'http://github.com/artemis-beta/enigma'       ,
+      author              =  'Kristian Zarebski'                           ,
+      author_email        =  'krizar312@yahoo.co.uk'                       ,
+      license             =  'MIT'                                         ,
+      packages            =  ['enigma']                                    ,
+      zip_safe            =  False                                         ,
+      tests_require       =  ['pytest']                                    ,
+      setup_requires      =  ['pytest-runner']                             ,
+     )
